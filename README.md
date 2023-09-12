66465f2946708dc95dee50207297c52b334bb5e8
FSH6088 <shuangliang.fu@baozun.com>
2023-09-12 16:28:30 +0800

    feat(init): :tada: init project

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+stdpath("data")路径为：
+macOS/Linux：~/.local/share/nvim
+Windows：~/AppData/Local/nvim-data
+```
+
+stdpath("data") 其实就是插件安装的目录
---
+++ lua/lazy-nvim/init.lua
@@ -0,0 +1,17 @@
+-- 设置获取路径，vim.fn.stdpath("data") 获取nvim-data目录
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "git@github.com:folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
