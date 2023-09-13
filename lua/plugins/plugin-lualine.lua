fbc224e91d0e188fc522d1d5d5caff90acf7cc9a
FSH6088 <shuangliang.fu@baozun.com>
2023-09-13 17:42:37 +0800

    feat: 更新搜索插件

--- lua/plugins/plugin-lualine.lua
+++ lua/plugins/plugin-lualine.lua
@@ -1,8 +1,8 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim',
-        config = function()
-          require('lualine').setup()
-        end
-      }
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup()
+    end
+  }
 }
